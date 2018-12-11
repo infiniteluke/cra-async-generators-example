0bd192854253679a9a84e50f556f810b610fbffa
Luke Herrington <lherrington100@gmail.com>
2018-12-10 16:18:24 -0800

    🔄 initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// @see https://jakearchibald.com/2017/async-iterators-and-generators/#async-generators-creating-your-own-async-iterator
+async function* asyncRandomNumbers() {
+  // This is a web service that returns a random number
+  const url = 'https://www.random.org/decimal-fractions/?num=1&dec=10&col=1&format=plain&rnd=new';
+
+  while (true) {
+    const response = await fetch(url);
+    const text = await response.text();
+    yield Number(text);
+  }
+}
+
 class App extends Component {
+  state = {
+    number: 0,
+    done: false,
+  }
+
+  async getRandomNumbers() {
+    for await (const number of asyncRandomNumbers()) {
+      this.setState({ number })
+      if (number > 0.95) {
+        this.setState({ done: true })
+        break;
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.getRandomNumbers();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{this.state.number}</h1>
+          {this.state.done ? <p>Found it</p> : <p>looking for a number greater than .95</p>}
         </header>
       </div>
     );
